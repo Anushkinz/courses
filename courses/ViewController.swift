5d66e26b45beec534a357b70a13eef234b7fa4eb
Anushkinz <anuarprogramm@gmail.com>
2022-02-19 14:33:50 +0600

    Initial Commit

---
+++ courses/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  courses
+//
+//  Created by anushkinz on 19/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
